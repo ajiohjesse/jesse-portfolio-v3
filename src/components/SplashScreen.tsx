2f49a25ab9b87876c7d42f34fd03293675904e4b
Jesse Ajioh <ajiohjesse@gmail.com>
2024-11-15 18:34:10 +0100

    chore: increase splash time

--- src/components/SplashScreen.tsx
+++ src/components/SplashScreen.tsx
@@ -11,7 +11,7 @@ const SplashScreen = () => {
   useEffect(() => {
     const activeTimeout = setTimeout(() => {
       setIsActive(false)
-    }, 2000)
+    }, 3000)
 
     return () => {
       clearTimeout(activeTimeout)
